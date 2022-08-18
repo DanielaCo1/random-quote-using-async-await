731aa195ead8ac009d8cdcae03ac80d030bdcccb
corda daniela <danielacorda.pro@gmail.com>
2022-08-18 09:27:39 +0200

    final Project

--- project.js
+++ project.js
@@ -16,30 +16,59 @@ let section = document.createElement("section");
 section.classList.add("author");
 let button = document.createElement("button");
 button.classList.add("button");
-button.innerHTML = "Next";
+button.innerHTML = "Next quote";
 button.addEventListener("click", (e) =>{
     nextQuote()
 })
 body.appendChild(button);
 
-
-
+// let age;
+let authorFirstName = ""
+let age
 function nextQuote(){
     getResponse().then(
         (response)=>{
-        console.log(response)
-        div.innerHTML = response.quote
-        body.appendChild(div);
-        if (response.photo ){
-            photo.setAttribute("src",response.photo)
-            body.appendChild(photo);
-        }
-        section.innerHTML = response.author
-        body.appendChild(section);
+            console.log(response)
+            div.innerHTML = response.quote
+            body.appendChild(div);
+            if (response.photo ){
+                photo.setAttribute("src",response.photo)
+                body.appendChild(photo);
+            }
+            let age
+            // authorFirstName = response.author.split(" ")[0]
+            // console.log(authorFirstName)
+            console.log(getAge(response.author.split(" ")[0]))
+            getAge(response.author.split(" ")[0]).then(function (agee){
+                age = agee
+                console.log(age)
+                section.innerHTML = response.author + " " + age;
+            }) 
+            body.appendChild(section);
         
         }
     ) 
 }
+function getAge (name) {
+    
+let url = "https://api.agify.io/?name=" + name;
+    
+return fetch (url)
+    .then((response) =>
+    {
+        return response.json()
+    })
+    .then((json) => {
+        console.log(json.age);
+        
+        return json.age
+    }) 
+    .catch((error) => {
+        console.log(error);
+    })
+}
+
+
+
 
 nextQuote();
-
